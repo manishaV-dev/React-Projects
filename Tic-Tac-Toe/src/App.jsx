f0dce8f0864f10925928df2688ac7019be53035f
Manisha varma <manishavkapex1@gmail.com>
2024-07-30 00:16:49 +0530

    make separate component for tic-tac-toe

--- Tic-Tac-Toe/src/App.jsx
+++ Tic-Tac-Toe/src/App.jsx
@@ -1,27 +1,11 @@
-import { useState } from "react";
 import "./App.css";
-
-const initialBoard = () => Array(9).fill(null);
+import TicTacToe from "./components/tic-tac-toe";
 
 function App() {
-  const [board, setBoard] = useState(initialBoard());
-  console.log(board); // (9) [null, null, null, null, null, null, null, null, null]
 
   return (
     <>
-      <div className="game">
-        <div className="status">
-          <h3 className="winBox">Player X Turn</h3>
-          <button className="resetBtn">Reset Game</button>
-        </div>
-        <div className="board">
-          {board.map((_, index) => (
-            <button className="cell" key={index}>
-              X
-            </button>
-          ))}
-        </div>
-      </div>
+      <TicTacToe />
     </>
   );
 }
---
+++ Tic-Tac-Toe/src/components/tic-tac-toe.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+const initialBoard = () => Array(9).fill(null);
+
+function TicTacToe() {
+  const [board, setBoard] = useState(initialBoard());
+  console.log(board); // (9) [null, null, null, null, null, null, null, null, null]
+
+  return (
+    <>
+      <div className="game">
+        <div className="status">
+          <h3 className="winBox">Player X Turn</h3>
+          <button className="resetBtn">Reset Game</button>
+        </div>
+        <div className="board">
+          {board.map((_, index) => (
+            <button className="cell" key={index}>
+              X
+            </button>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default TicTacToe;
