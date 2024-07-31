5b99896b70303c6205ea569c032de125e0341f0e
Manisha varma <manishavkapex1@gmail.com>
2024-07-31 15:13:10 +0530

    complete logic for shuffle list items

--- Mini-Projects/src/components/ShuffleList.jsx
+++ Mini-Projects/src/components/ShuffleList.jsx
@@ -1,30 +1,50 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 function ShuffleList() {
+  const [items, setItems] = useState([
+    "Banana",
+    "Orange",
+    "Melon",
+    "Kiwi",
+    "Papaya",
+  ]);
 
-    const [items, setItems] = useState([
-        "Banana",
-        "Orange",
-        "Melon",
-        "Kiwi",
-        "Papaya"
-    ])
+  /*
+Logic -- Traverse over the list of items and for each particular item I'll genarate a random index and swap that particular item with that particular index. and set state again.
+
+    */
+  const handleShuffle = () => {
+    const itemsShallowCopy = [...items];
+
+    // reverse loop
+    for (let i = itemsShallowCopy.length - 1; i > 0; i--) {
+      // generate random index
+      const j = Math.floor(Math.random() * (i + 1));
+      console.log(j);
+      // swap two items
+      [itemsShallowCopy[i], itemsShallowCopy[j]] = [
+        itemsShallowCopy[j],
+        itemsShallowCopy[i],
+      ];
+    }
+    setItems(itemsShallowCopy);
+  };
 
   return (
     <>
-    <div className="list-box">
+      <div className="list-box">
         <ul>
-            {
-                items.map((item, index) => (
-                    <li key={index}>{item}</li>
-                ))
-            }
+          {items.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
         </ul>
 
-        <button className='submitBtn'>Shuffle List</button>
-    </div>
+        <button className="submitBtn" onClick={handleShuffle}>
+          Shuffle List
+        </button>
+      </div>
     </>
-  )
+  );
 }
 
-export default ShuffleList
+export default ShuffleList;
