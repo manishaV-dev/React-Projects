140908b5312033f594dbe4a45fb0827ef899a650
Manisha varma <manishavkapex1@gmail.com>
2024-09-18 19:02:24 +0530

    mention type of store and dispatch

--- Habit-tracker-reduxToolkit/src/store/store.ts
+++ Habit-tracker-reduxToolkit/src/store/store.ts
@@ -1,5 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit'
+import habitReducers from './habit-slice'
 
-export default configureStore({
-    reducer: {}
+const store = configureStore({
+    reducer: {
+        habits: habitReducers
+    }
 })
+
+// type of store
+export type RootState = ReturnType<typeof store.getState>
+
+// type of dispatch
+export type AppDispatch = typeof store.dispatch
+
+export default store
