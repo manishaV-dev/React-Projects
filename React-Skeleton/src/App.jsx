4342e2a3283054d60e3dbcb84ae2e46773cf207f
Manisha varma <manishavkapex1@gmail.com>
2024-12-22 23:11:17 +0530

    add heading content and add loading state

--- React-Skeleton/src/App.jsx
+++ React-Skeleton/src/App.jsx
@@ -1,19 +1,38 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import { useEffect, useState } from "react";
 import "./App.css";
 import Skeleton from "./skeleton/Skeleton";
 
 function App() {
+  const [loadingState, setLoadingState] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoadingState(true);
+    }, 2000);
+  }, []);
+
   return (
     <>
       <h1>React Skeleton Loading...</h1>
       <div>
         <h2>Heading Skeleton</h2>
-        <div className="skeleton-container">
-          <Skeleton width={`200px`} height={`38px`} />
-          <Skeleton width={`150px`} height={`28px`} />
-          <Skeleton width={`100px`} height={`25px`} />
-        </div>
+
+        {loadingState ? (
+          <div className="heading">
+            <h1>Heading 1</h1>
+            <h2>Heading 2</h2>
+            <h3>Heading 3</h3>
+          </div>
+        ) : (
+          <div className="skeleton-container">
+            <Skeleton width={`200px`} height={`38px`} />
+            <Skeleton width={`150px`} height={`28px`} />
+            <Skeleton width={`100px`} height={`25px`} />
+          </div>
+        )}
+
         <h2>Content Skeleton</h2>
         <div className="content-skeleton">
           <div className="skeleton-container">
@@ -59,8 +78,6 @@ function App() {
             <div className="cardBody">
               <Skeleton width={`250px`} height={`300px`} />
             </div>
-
-
           </div>
 
           <div className="cardSkeleton">
@@ -76,8 +93,6 @@ function App() {
             <div className="cardBody">
               <Skeleton width={`250px`} height={`300px`} />
             </div>
-
-
           </div>
 
           <div className="cardSkeleton">
@@ -93,8 +108,6 @@ function App() {
             <div className="cardBody">
               <Skeleton width={`250px`} height={`300px`} />
             </div>
-
-
           </div>
 
           <div className="cardSkeleton">
@@ -110,8 +123,6 @@ function App() {
             <div className="cardBody">
               <Skeleton width={`250px`} height={`300px`} />
             </div>
-
-
           </div>
         </div>
       </div>
