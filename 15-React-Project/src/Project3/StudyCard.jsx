a399387d5428b953045254dac8f5362ea2f9ed1b
Manisha varma <manishavkapex1@gmail.com>
2024-12-31 14:11:22 +0530

    add display and close functionality to the card

--- 15-React-Project/src/Project3/StudyCard.jsx
+++ 15-React-Project/src/Project3/StudyCard.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./studyCard.css";
 import quizQuestions from "./data";
 
 function StudyCard() {
+  // using null- because first want to show only question
+  // const [selectedId, setSelectedId] = useState('FC4IBr');
+  const [selectedId, setSelectedId] = useState(null);
+
+  //   when click on card only shows the answer
+
+  function handleClick(id) {
+    // when click on card it display answer and by clicking again it has to again shows only the question
+    setSelectedId(id !== selectedId ? id : "");
+  }
   return (
     <>
-      <h3>Study Flash Card</h3>
+      <h2>Study Flash Card</h2>
       <div className="card-grid">
         {quizQuestions.map((que) => (
-          <div className="card">{que.question}</div>
+          <div
+            className={`card ${que.id === selectedId ? "active" : ""}`}
+            onClick={() => handleClick(que.id)}
+            key={que.id}
+          >
+            {que.id === selectedId ? que.answer : que.question}
+          </div>
         ))}
       </div>
     </>
