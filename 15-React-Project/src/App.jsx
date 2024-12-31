41a99c8d3052db768f66ae97846dea57cf2d1565
Manisha varma <manishavkapex1@gmail.com>
2024-12-31 13:47:02 +0530

    create study flash card component and add data object

--- 15-React-Project/src/App.jsx
+++ 15-React-Project/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 // import Product from './Project1/Product'
-import RandomProduct from './project2/RandomProduct'
+// import RandomProduct from './project2/RandomProduct'
+import StudyCard from './Project3/StudyCard'
 
 function App() {
   return (
     <>
     {/* <Product /> */}
-    <RandomProduct />
+    {/* <RandomProduct /> */}
+    <StudyCard />
     </>
   )
 }
---
+++ 15-React-Project/src/Project3/StudyCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function StudyCard() {
+  return (
+    <>
+    
+    <h3>Study Flash Card</h3>
+    </>
+  )
+}
+
+export default StudyCard
