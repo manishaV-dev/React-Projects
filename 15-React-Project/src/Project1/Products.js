e2a41178852015262a7e40e44fa22854595ec8d5
Manisha varma <manishavkapex1@gmail.com>
2024-12-31 11:18:56 +0530

    create array of product object

---
+++ 15-React-Project/src/Project1/Products.js
@@ -0,0 +1,81 @@
+export const products = [
+  {
+    productName: "Creamy Cake",
+    productImage: "../assets/cake.png",
+    productDescription: "Deliciously moist cake slice",
+    productPrice: "19.99",
+    stock: true,
+  },
+
+  {
+    productName: "Cheese Cake",
+    productImage: "../assets/cheesecake.png",
+    productDescription: "Deliciously moist cheese cake slice",
+    productPrice: "39.09",
+    stock: true,
+  },
+
+  {
+    productName: "Chocolate Bar",
+    productImage: "../assets/chocolate-bar.png",
+    productDescription: "Decadent Dark chocolate bar",
+    productPrice: "18.58",
+    stock: true,
+  },
+
+  {
+    productName: "Fast Food Box",
+    productImage: "../assets/fast-food.png",
+    productDescription: "A lunch box of delecious fast food ",
+    productPrice: "25.87",
+    stock: true,
+  },
+
+  {
+    productName: "Flower",
+    productImage: "../assets/flower.png",
+    productDescription: "A flower for your loved ones",
+    productPrice: "10.89",
+    stock: true,
+  },
+
+  {
+    productName: "Fruit Basket",
+    productImage: "../assets/fruit.png",
+    productDescription: "A basket of healthy fruits",
+    productPrice: "25.54",
+    stock: false,
+  },
+
+  {
+    productName: "Gift Items",
+    productImage: "../assets/gifts.png",
+    productDescription: "A gift for your loved ones",
+    productPrice: "69.99",
+    stock: true,
+  },
+
+  {
+    productName: "Heart Baloon ",
+    productImage: "../assets/heart.png",
+    productDescription: "Beautiful gift box full of heart shape baloon",
+    productPrice: "21.84",
+    stock: false,
+  },
+
+  {
+    productName: "Ice Cream",
+    productImage: "../assets/ice-cream.png",
+    productDescription: "Delicious vanila flavour ice cream",
+    productPrice: "15.63",
+    stock: true,
+  },
+
+  {
+    productName: "Makeup Kit",
+    productImage: "../assets/makeup.png",
+    productDescription: "Makeup kit for your loved ones",
+    productPrice: "45.36",
+    stock: true,
+  },
+];
