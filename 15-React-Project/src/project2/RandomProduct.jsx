6aae31b98ba1b889a8615bf479b192d6ec63c532
Manisha varma <manishavkapex1@gmail.com>
2024-12-31 13:01:19 +0530

    add error and loading state

--- 15-React-Project/src/project2/RandomProduct.jsx
+++ 15-React-Project/src/project2/RandomProduct.jsx
@@ -1,13 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import "./product.css";
 
 function RandomProduct() {
-  const [productTitle, setProductTitle] = useState([]);
+  const [productTitle, setProductTitle] = useState("");
   const [productImage, setProductImage] = useState(null);
   const [count, setCount] = useState(0);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getProduct();
+  }, []);
 
   const getProduct = async () => {
+    setLoading(true);
     try {
       const response = await fetch("https://fakestoreapi.com/products");
+      if (!response.ok) throw new Error("Failed to fetch products");
+
       const data = await response.json();
       //   console.log(data);
 
@@ -19,8 +29,12 @@ function RandomProduct() {
 
       setProductTitle(randomProduct.title);
       setProductImage(randomProduct.image);
+      setError(null); // Clear any previous error
     } catch (error) {
       console.log("Something went wrong", error);
+      setError("Unable to fetch product. Please try again.");
+    } finally {
+      setLoading(false);
     }
 
     setCount((prev) => prev + 1);
@@ -29,13 +43,20 @@ function RandomProduct() {
   return (
     <>
       <h2>Generate Random Product</h2>
-      <div className="product">
-        <h3 className="title">{productTitle}</h3>
-        <img src={productImage} alt={productTitle} />
-
-        <p className="count">You have seen {count} product</p>
-
-        <button onClick={getProduct}>Add to cart</button>
+      <div className="product-container">
+        <div className="product">
+          {error && <p className="error-message">{error}</p>}
+          <h5 className="title">{productTitle}</h5>
+          {productImage && <img src={productImage} alt={productTitle} />}
+          <p className="count">
+            You have seen {count} product{count !== 1 && "s"}
+          </p>
+          {loading ? (
+            <button>Loading...</button>
+          ) : (
+            <button onClick={getProduct}>Generate random product</button>
+          )}
+        </div>
       </div>
     </>
   );
