0ab925c4721c735ebb738394db669cd62364e603
Manisha varma <manishavkapex1@gmail.com>
2024-12-31 12:31:44 +0530

    show title and image using useState hook

--- 15-React-Project/src/project2/RandomProduct.jsx
+++ 15-React-Project/src/project2/RandomProduct.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 function RandomProduct() {
+  const [productTitle, setProductTitle] = useState([]);
+  const [productImage, setProductImage] = useState(null);
+  const [count, setCount] = useState(0);
+
   const getProduct = async () => {
     try {
       const response = await fetch("https://fakestoreapi.com/products");
@@ -12,20 +16,24 @@ function RandomProduct() {
 
       const randomProduct = data[randomNum];
       //   console.log(randomProduct);
-      
+
+      setProductTitle(randomProduct.title);
+      setProductImage(randomProduct.image);
     } catch (error) {
       console.log("Something went wrong", error);
     }
+
+    setCount((prev) => prev + 1);
   };
 
   return (
     <>
       <h2>Generate Random Product</h2>
       <div className="product">
-        <h3 className="title"></h3>
-        <img src="" alt="" />
+        <h3 className="title">{productTitle}</h3>
+        <img src={productImage} alt={productTitle} />
 
-        <p className="count">You have seen 1 product</p>
+        <p className="count">You have seen {count} product</p>
 
         <button onClick={getProduct}>Add to cart</button>
       </div>
