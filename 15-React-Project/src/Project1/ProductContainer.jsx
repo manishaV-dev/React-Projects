4c32b96d573de4c3b438eee60f81f6b86fbbaa5b
Manisha varma <manishavkapex1@gmail.com>
2024-12-31 11:38:31 +0530

    show all products in our ui

--- 15-React-Project/src/Project1/ProductContainer.jsx
+++ 15-React-Project/src/Project1/ProductContainer.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 import ProductDetails from "./ProductDetails";
+import { products } from "./Products";
 
 function ProductContainer() {
   return (
     <>
       <main>
         <section className="products">
-          <ProductDetails />
-          <ProductDetails />
-          <ProductDetails />
-          <ProductDetails />
-          <ProductDetails />
-          <ProductDetails />
+          {products.map((product, index) => (
+            <ProductDetails productObj={product} key={index} />
+          ))}
         </section>
       </main>
     </>
--- 15-React-Project/src/Project1/ProductDetails.jsx
+++ 15-React-Project/src/Project1/ProductDetails.jsx
@@ -1,20 +1,29 @@
 import React from "react";
 
-function ProductDetails() {
+function ProductDetails({ productObj }) {
+  // console.log(productObj);
+
+  const { productName, productImage, productDescription, productPrice, stock } =
+    productObj;
+
   return (
     <>
       <div className="product">
-        <img src="../assets/cake.png" alt="" />
+        <img src={productImage} alt={productName} />
         <div className="product-about">
-          <h4>Cream Cake</h4>
-          <p className="price">$ 19.90</p>
+          <h4> {productName} </h4>
+          <p className="price">$ {productPrice}</p>
         </div>
 
-        <p className="product-details">
-          Lorem ipsum dolor sit amet sit amet sit
-        </p>
+        <p className="product-details">{productDescription}</p>
 
-        <button>Add to cart</button>
+        {stock ? (
+          <button>Add to cart</button>
+        ) : (
+          <button className="outStock" disabled={true}>
+            Out of stock
+          </button>
+        )}
       </div>
     </>
   );
