386f88378e6058308c68b357e675da9dca5f22b1
Manisha varma <manishavkapex1@gmail.com>
2025-01-01 20:41:51 +0530

    create accordion and accordionItem component

--- 15-React-Project/src/project4/Accordion.jsx
+++ 15-React-Project/src/project4/Accordion.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./accordion.css";
+import AccordionItem from "./AccordionItem";
 
 function Accordion() {
   return (
     <>
-      <div className="accordion-container"></div>
+      <div className="accordion-container">
+        <h3 className="faq">Frequently Asked Questions (FAQ's)</h3>
+        <AccordionItem />
+      </div>
     </>
   );
 }
--- 15-React-Project/src/project4/AccordionItem.jsx
+++ 15-React-Project/src/project4/AccordionItem.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
 
 function AccordionItem() {
   return (
     <>
-    
+      <div className="accordion">
+        <div className="accordion-header">
+          <span className="accordion-icon">+</span>
+          <span className="accordion-number">01</span>
+          <span className="accordion-title">What is Javascript</span>
+        </div>
+
+        <div className="accordion-content">
+          <p>It is high level programming language</p>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default AccordionItem
+export default AccordionItem;
