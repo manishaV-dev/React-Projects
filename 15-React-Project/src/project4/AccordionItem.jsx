380d84e807ee6649dec3b8040ec68bbb31ab1096
Manisha varma <manishavkapex1@gmail.com>
2025-01-01 20:59:57 +0530

    complete first accordion functionality

--- 15-React-Project/src/project4/AccordionItem.jsx
+++ 15-React-Project/src/project4/AccordionItem.jsx
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 function AccordionItem({ item, number }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleToggle() {
+    // setIsOpen(!isOpen);
+    // both are right
+    setIsOpen((isOpen) => !isOpen);
+  }
+
   return (
     <>
-      <div className="accordion">
+      <div
+        className={`accordion ${isOpen ? "active" : ""}`}
+        onClick={handleToggle}
+      >
         <div className="accordion-header">
-          <span className="accordion-icon">+</span>
+          <span className="accordion-icon"> {isOpen ? "-" : "+"} </span>
           <span className="accordion-number">
             {number < 9 ? `0${number + 1}` : number + 1}
           </span>
           <span className="accordion-title">{item.question}</span>
         </div>
 
-        <div className="accordion-content">
-          <p>{item.answer}</p>
-        </div>
+        {isOpen && (
+          <div className="accordion-content">
+            <p>{item.answer}</p>
+          </div>
+        )}
       </div>
     </>
   );
