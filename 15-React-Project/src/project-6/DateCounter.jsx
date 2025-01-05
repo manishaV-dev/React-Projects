b4c2dcfb614978413ba22e86d53b98841b41f15c
Manisha varma <manishavkapex1@gmail.com>
2025-01-05 23:27:51 +0530

    complete

--- 15-React-Project/src/project-6/DateCounter.jsx
+++ 15-React-Project/src/project-6/DateCounter.jsx
@@ -5,6 +5,16 @@ const DateCounter = () => {
   const [count, setCount] = useState(0);
   const [step, setStep] = useState(1);
 
+  const date = new Date("25 Nov 2025");
+  //   console.log(date);
+  date.setDate(date.getDate() + count);
+  //   console.log(date);
+
+  const handleReset = () => {
+    setCount(0);
+    setStep(1);
+  };
+
   return (
     <>
       <h2>Display Date using useState</h2>
@@ -16,24 +26,45 @@ const DateCounter = () => {
               min="0"
               max="10"
               className="range-input"
-              value=""
+              value={step}
+              onChange={(e) => setStep(+e.target.value)}
             />
             <span className="step-label">Step : {step}</span>
           </div>
 
           <div className="counter-container">
-            <button className="btn">-</button>
-            <input type="text" className="counter-input" value="" />
-            <button className="btn">+</button>
+            <button className="btn" onClick={() => setCount((c) => c - step)}>
+              -
+            </button>
+            <input
+              type="text"
+              className="counter-input"
+              value={count}
+              onChange={(e) => setCount(+e.target.value)}
+            />
+            <button className="btn" onClick={() => setCount((c) => c + step)}>
+              +
+            </button>
           </div>
 
           <p className="date-display">
-            <span></span>
-            <span></span>
+            <span>
+              {count === 0
+                ? "Today is"
+                : count > 0
+                ? `${count} days from today is`
+                : `${Math.abs(count)} days ago was`}{" "}
+              {/* // to ignore the negative number */}
+            </span>
+            <span>{date.toDateString()}</span>
           </p>
 
           <div>
-            <button className="btn reset-btn">Reset</button>
+            {count !== 0 || step !== 1 ? (
+              <button className="btn reset-btn" onClick={handleReset}>
+                Reset
+              </button>
+            ) : null}
           </div>
         </div>
       </div>
