1b5dc3b41bb13e5bb7da348a3b87d6c4a75e203e
Manisha varma <manishavkapex1@gmail.com>
2025-01-19 21:00:36 +0530

    add css

--- react-hook-form/src/App.jsx
+++ react-hook-form/src/App.jsx
@@ -1,12 +1,37 @@
-import './App.css'
+import "./App.css";
+import { useForm } from "react-hook-form";
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
 
   return (
     <>
       <h2>React-Hook-Form</h2>
+      <div className="form-container">
+        <form>
+          <div  className="form-group">
+            <label>First Name :</label>
+            <input />
+          </div>
+
+          <div  className="form-group">
+            <label>Middle Name :</label>
+            <input />
+          </div>
+
+          <div className="form-group">
+            <label>Last Name :</label>
+            <input />
+          </div>
+        </form>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
