24c8aac677fde583f1f55a32d2f74a285a15f40e
Manisha varma <manishavkapex1@gmail.com>
2025-01-19 21:53:59 +0530

    complete

--- react-hook-form/src/App.jsx
+++ react-hook-form/src/App.jsx
@@ -5,29 +5,50 @@ function App() {
   const {
     register,
     handleSubmit,
-    watch,
-    formState: { errors },
+    formState: { errors, isSubmitting },
   } = useForm();
+
+  const onSubmit = async (data) => {
+    await new Promise((res) => setTimeout(res, 5000));
+    console.log("Submitting the form", data);
+  };
 
   return (
     <>
       <h2>React-Hook-Form</h2>
       <div className="form-container">
-        <form>
-          <div  className="form-group">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="form-group">
             <label>First Name :</label>
-            <input />
+            <input
+              className={errors.firstName ? "error-input" : ""}
+              {...register("firstName", {
+                required: true,
+                minLength: { value: 5, message: "Min Length 15" },
+              })}
+            />
+            {errors.firstName && <p> {errors.firstName.message} </p>}
           </div>
 
-          <div  className="form-group">
+          <div className="form-group">
             <label>Middle Name :</label>
-            <input />
+            <input
+              className={errors.middleName ? "error-input" : ""}
+              {...register("middleName", { required: true })}
+            />
           </div>
 
           <div className="form-group">
             <label>Last Name :</label>
-            <input />
+            <input
+              className={errors.lastName ? "error-input" : ""}
+              {...register("lastName", { required: true })}
+            />
           </div>
+
+          <button type="submit" disabled={isSubmitting}>
+            {isSubmitting ? "Submitting" : "Submit"}
+          </button>
         </form>
       </div>
     </>
