45cac39795d98d8fe637bb744198fe3a989fe6e4
Manisha varma <manishavkapex1@gmail.com>
2025-01-25 20:52:47 +0530

    config data

--- 15-React-Project/src/10-Timer/Timer.jsx
+++ 15-React-Project/src/10-Timer/Timer.jsx
@@ -1,25 +1,60 @@
-import React from "react";
-import './timer.css'
+import React, { useState } from "react";
+import "./timer.css";
+
+const TimeFactor = {
+  Hour: "hh",
+  Minute: "mm",
+  Seconds: "ss",
+  MilliSeconds: "ms",
+};
+
+const Config = {
+  [TimeFactor.Hour]: {
+    value: 0,
+    factor: 60 * 60 * 1000,
+    placeholder: "HH",
+  },
+  [TimeFactor.Minute]: {
+    value: 0,
+    factor: 60 * 1000,
+    placeholder: "MM",
+  },
+  [TimeFactor.Seconds]: {
+    value: 0,
+    factor: 1000,
+    placeholder: "SS",
+  },
+};
+
+const OrderOfTime = [
+  TimeFactor.Hour,
+  TimeFactor.Minute,
+  TimeFactor.Seconds,
+  TimeFactor.MilliSeconds,
+];
 
 const Timer = () => {
-  return <>
-  
-  <div className="timer">
-    <div className="text-fields">
-        <input type="text" placeholder="HH" />
-        <span>:</span>
-        <input type="text" placeholder="MM" />
-        <span>:</span>
-        <input type="text" placeholder="SS" />
-    </div>
+  const [config, setConfig] = useState(structuredClone(Config)); // structuredClone -- for making deep copy
 
-    <div className="buttons">
-        <button>&#9654; Play</button>
-        <button>&#x23f8; Pause</button>
-        <button>&#x23f1; Reset</button>
-    </div>
-  </div>
-  </>;
+  return (
+    <>
+      <div className="timer">
+        <div className="text-fields">
+          <input type="text" placeholder="HH" />
+          <span>:</span>
+          <input type="text" placeholder="MM" />
+          <span>:</span>
+          <input type="text" placeholder="SS" />
+        </div>
+
+        <div className="buttons">
+          <button>&#9654; Play</button>
+          <button>&#x23f8; Pause</button>
+          <button>&#x23f1; Reset</button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Timer;
