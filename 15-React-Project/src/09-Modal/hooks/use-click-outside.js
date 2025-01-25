2c6d63715589b72f85822262f019d8fda76c9446
Manisha varma <manishavkapex1@gmail.com>
2025-01-25 20:15:04 +0530

    show modal on click

--- 15-React-Project/src/09-Modal/hooks/use-click-outside.js
+++ 15-React-Project/src/09-Modal/hooks/use-click-outside.js
@@ -3,15 +3,19 @@ import { useEffect } from "react";
 export default function useClickOutside(elementRef, handler) {
   useEffect(() => {
     const cb = (e) => {
-      if (!elementRef.current?.contains(e.target)) {
+      // Check if the click is outside the modal element
+      if (elementRef.current && !elementRef.current?.contains(e.target)) {
         // ye check kr rha hai ki modal pr click nhi hona chahiye agar aisa hai to handler ko call kr dena hai
         handler();
       }
     };
-    document.addEventListener("click", cb); // otherwise jaha bhi click ho modal close ho jaye
+    // document.addEventListener("click", cb); // otherwise jaha bhi click ho modal close ho jaye
+
+    document.addEventListener("mousedown", cb);
 
     return () => {
-      document.removeEventListener("click", cb);
+      // document.removeEventListener("click", cb);
+      document.removeEventListener("mousedown", cb);
     };
   }, [elementRef, handler]);
 }
