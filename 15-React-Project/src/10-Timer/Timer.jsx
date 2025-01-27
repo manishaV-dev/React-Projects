a4186dc1df3c9d4b6c6c629bb9a16601c93cfad4
Manisha varma <manishavkapex1@gmail.com>
2025-01-27 23:13:32 +0530

    timer complete

--- 15-React-Project/src/10-Timer/Timer.jsx
+++ 15-React-Project/src/10-Timer/Timer.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import "./timer.css";
 
 const TimeFactor = {
@@ -10,47 +10,113 @@ const TimeFactor = {
 
 const Config = {
   [TimeFactor.Hour]: {
-    value: 0,
+    value: "",
     factor: 60 * 60 * 1000,
     placeholder: "HH",
   },
   [TimeFactor.Minute]: {
-    value: 0,
+    value: "",
     factor: 60 * 1000,
     placeholder: "MM",
   },
   [TimeFactor.Seconds]: {
-    value: 0,
+    value: "",
     factor: 1000,
     placeholder: "SS",
   },
+  //   [TimeFactor.MilliSeconds]: {
+  //     value: "",
+  //     factor: 1,
+  //     placeholder: "ms",
+  //   },
 };
 
-const OrderOfTime = [
-  TimeFactor.Hour,
-  TimeFactor.Minute,
-  TimeFactor.Seconds,
-  TimeFactor.MilliSeconds,
-];
+const OrderOfTime = [TimeFactor.Hour, TimeFactor.Minute, TimeFactor.Seconds];
 
 const Timer = () => {
   const [config, setConfig] = useState(structuredClone(Config)); // structuredClone -- for making deep copy
+
+  const [time, setTime] = useState(0);
+
+  const intervalRef = useRef(null);
+  const timeSpanRef = useRef(0);
+
+  function handleChange({ key }) {
+    return (e) => {
+      const newConfig = structuredClone(config);
+      newConfig[key].value = e.target.value;
+      setConfig(newConfig);
+    };
+  }
+
+  function handleStart() {
+    // convert entered time in ms
+    let totalTimeInMs = 0;
+
+    OrderOfTime.forEach((key) => {
+      const data = config[key];
+      const value = data.value;
+      const factor = data.factor;
+
+      if (value && !isNaN(value)) {
+        totalTimeInMs += Number(value) * factor;
+      }
+    });
+
+    // start the timer
+    timeSpanRef.current = Date.now() + totalTimeInMs;
+    intervalRef.current = setInterval(() => {
+      setTime(() => timeSpanRef.current - new Date().getTime());
+    }, 10);
+  }
+
+  function handlePause() {
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+  }
+
+  function handleReset() {
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+    setTime(0);
+    timeSpanRef.current = 0;
+    setConfig(structuredClone(Config));
+  }
+
+  function formattedTime() {
+    const ms = Math.floor((time % 1000) / 10); // 1234 -> is 234ms/10 2.34
+    const mm = Math.floor((time / (60 * 1000)) % 60);
+    const ss = Math.floor((time / 1000) % 60);
+    const hh = Math.floor(time / (60 * 60 * 1000));
+
+    return `${hh} : ${mm} : ${ss} : ${ms}`;
+  }
 
   return (
     <>
       <div className="timer">
         <div className="text-fields">
-          <input type="text" placeholder="HH" />
-          <span>:</span>
-          <input type="text" placeholder="MM" />
-          <span>:</span>
-          <input type="text" placeholder="SS" />
+          {OrderOfTime.map((orderKey, index) => {
+            const data = config[orderKey];
+            // console.log(data)
+            return (
+              <div key={orderKey}>
+                <input
+                  type="text"
+                  placeholder={data.placeholder}
+                  value={data.value}
+                  onChange={handleChange({ key: orderKey, index })}
+                />
+              </div>
+            );
+          })}
         </div>
 
+        <h3>{formattedTime()}</h3>
         <div className="buttons">
-          <button>&#9654; Play</button>
-          <button>&#x23f8; Pause</button>
-          <button>&#x23f1; Reset</button>
+          <button onClick={handleStart}>&#9654; Play</button>
+          <button onClick={handlePause}>&#x23f8; Pause</button>
+          <button onClick={handleReset}>&#x23f1; Reset</button>
         </div>
       </div>
     </>
