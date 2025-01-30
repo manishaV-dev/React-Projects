15a04389ffc9351cf8eba39e2db910327e0ce28e
Manisha varma <manishavkapex1@gmail.com>
2025-01-30 13:11:23 +0530

    use the context

--- Advance-Filter/src/context/FilterContext.tsx
+++ Advance-Filter/src/context/FilterContext.tsx
@@ -44,4 +44,14 @@ export const FilterProvider: React.FC<{ children: ReactNode }> = ({
   );
 };
 
+export const useFilter = () => {
+  const context = useContext(FilterContext);
+
+  if (context === undefined) {
+    throw new Error("useFilter must be used within a filterProvider");
+  }
+
+  return context;
+};
+
 export default FilterContext;
