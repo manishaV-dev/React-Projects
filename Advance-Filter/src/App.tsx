02db68ea98e1abf3c32b514533c1527eeb9ca865
Manisha varma <manishavkapex1@gmail.com>
2025-01-30 15:15:48 +0530

    create product details page

--- Advance-Filter/src/App.tsx
+++ Advance-Filter/src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import MainContent from "./components/MainContent";
+import ProductPage from "./components/ProductPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <div className="rounded w-full flex justify-between flex-wrap">
             <Routes>
               <Route path="/" element={<MainContent />} />
+              <Route path="/product/:id" element={<ProductPage />} />
             </Routes>
           </div>
         </div>
