7b91f87bd870d1370254b7b0f0dd449a799c994c
Manisha varma <manishavkapex1@gmail.com>
2025-02-26 15:49:18 +0530

    add filter

--- oodles/src/components/TaskList.jsx
+++ oodles/src/components/TaskList.jsx
@@ -7,9 +7,32 @@ const TaskList = () => {
     { title: "Task 2", status: "Completed" },
   ]);
 
+  const [filterTask, setFilterTask] = useState("All");
+
   const addTask = (newTask) => {
     setTasks([...tasks, newTask]);
   };
+
+  const handleToggleState = (index) => {
+    setTasks(
+      tasks.map((task, i) =>
+        i === index
+          ? {
+              ...task,
+              status: task.status === "Pending" ? "Completed" : "Pending",
+            }
+          : task
+      )
+    );
+  };
+
+  const handleDelete = (index) => {
+    setTasks(tasks.filter((_, i) => i !== index));
+  };
+
+  const FilterData = tasks.filter(
+    (task) => filterTask === "All" || task.status === filterTask
+  );
 
   return (
     <>
@@ -17,8 +40,21 @@ const TaskList = () => {
 
       <AddTask addTask={addTask} />
 
+      <div>
+        <label htmlFor="">Filter Task</label>
+        <select
+          value={filterTask}
+          onChange={(e) => setFilterTask(e.target.value)}
+        >
+          <option value="all">All</option>
+          <option value="Pending">Pending</option>
+          <option value="Completed">Completed</option>
+        </select>
+      </div>
+
       <div style={{ width: "500px" }}>
-        {tasks.map((task, index) => (
+        {/* {tasks.map((task, index) => ( */}
+        {FilterData.map((task, index) => (
           <div
             key={index}
             style={{
@@ -28,8 +64,15 @@ const TaskList = () => {
             }}
             className="card"
           >
-            <p>{task.title}</p>
-            <button>{task.status}</button>
+            {task.status === "Completed" ? (
+              <p style={{ textDecoration: "line-through" }}>{task.title}</p>
+            ) : (
+              <p>{task.title}</p>
+            )}
+            <button onClick={() => handleToggleState(index)}>
+              {task.status}
+            </button>{" "}
+            <button onClick={() => handleDelete(index)}>Delete</button>
           </div>
         ))}
       </div>
